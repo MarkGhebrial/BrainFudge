9320cec2ca82afc4f977d89d6eaed837743f77ba
MarkGhebrial <45022215+MarkGhebrial@users.noreply.github.com>
2021-12-09 21:47:30 +0000

    Hell yeah, the interpreter works!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
+    // The Brainf*** source code
     let input = String::from("++>+++++[<+>-]++++++++[<++++++>-]<.");
 
+    // Parse each symbol of the source code
     let mut commands: Vec<char> = Vec::new();
     for c in input.chars() {
         if vec!('>', '<', '+', '-', '.', ',', '[', ']').contains(&c) {
@@ -11,55 +13,60 @@ fn main() {
     let mut data_pointer: usize = 0;
     let mut memory: Vec<u8> = vec!();
 
-    // Set the memory address specified by pointer to
-    // equal new_value, expanding the memory stack if
-    // it's too small
-    /*let mut set_address = |pointer: &usize, new_val: &u8| {
-        // Check if the memory stack is too short
-        if &memory.len() <= &*pointer {
-            // Extend the memory
-            &memory.resize(pointer + 1, 0);
-        }
-        &mut memory[*pointer] = &mut *new_val;
-    };*/
-
-    /*let get_address = |pointer: &usize| -> u8 {
-        match &memory.get(*pointer) {
-            Some(x) => **x,
-            None => 0
-        }
-    };*/
-
-    for i in 0..commands.len() {
-        println!("{}", commands[i]);
+    let mut i = 0;
+    'main_loop: while i < commands.len() {
         match commands[i] {
             '>' => {
+                // Move the pointer forward
                 data_pointer += 1;
             }
             '<' => {
+                // Move it backward
                 data_pointer -= 1;
             }
             '+' => {
+                // Increment the currently pointed byte by one
                 let current_value = get_address(&mut memory, &data_pointer);
                 set_address(&mut memory, &data_pointer, &mut(current_value + 1));
             }
             '-' => {
+                // Decrement the currently pointed byte by one
                 let current_value = get_address(&mut memory, &data_pointer);
                 set_address(&mut memory, &data_pointer, &mut(current_value - 1));
             }
             '.' => {
-                print!("{}", get_address(&mut memory, &data_pointer));
+                // Convert the pointed byte to ASCII and print it
+                print!("{}", char::from(get_address(&mut memory, &data_pointer)));
             }
             ',' => {
                 //TODO: implement this
             }
-            '[' => {}
-            ']' => {}
-            _ => {}
+            '[' => {
+                // Jump foward to the next ']' if the current byte is non-zero
+                if get_address(&mut memory, &data_pointer) == 0 {
+                    for j in i..commands.len() {
+                        if commands[j] == ']' {
+                            i = j + 1;
+                            continue 'main_loop; // Skip incrementing the current command
+                        }
+                    }
+                    panic!("Unmatched '[' at position {}", i);
+                }
+            }
+            ']' => {
+                // Jump back to the previous '['
+                for j in (0..i).rev() {
+                    if commands[j] == '[' { // Move backwards through the commands
+                        i = j;
+                        continue 'main_loop;
+                    }
+                }
+                panic!("Unmatched '[' at position {}", i);
+            }
+            _ => {} // Cover all cases
         }
+        i += 1;
     }
-
-    println!("{:#?}", memory);
 }
 
 fn set_address (memory: &mut Vec<u8>, pointer: &usize, new_val: &mut u8) {
