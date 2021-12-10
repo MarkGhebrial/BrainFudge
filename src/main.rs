4f037ceb135da4bc970178c9732aae09b301775a
MarkGhebrial <45022215+MarkGhebrial@users.noreply.github.com>
2021-12-10 02:28:09 +0000

    Refactor memory management

--- src/main.rs
+++ src/main.rs
@@ -14,40 +14,37 @@ fn main() {
         }
     }
 
-    let mut data_pointer: usize = 0;
-    let mut memory: Vec<u8> = vec!();
+    let mut memory = BFMemory::new();
 
     let mut i = 0;
     'main_loop: while i < commands.len() {
         match commands[i] {
             '>' => {
                 // Move the pointer forward
-                data_pointer += 1;
+                memory.increment_pointer(&1);
             }
             '<' => {
                 // Move it backward
-                data_pointer -= 1;
+                memory.decrement_pointer(&1);
             }
             '+' => {
                 // Increment the currently pointed byte by one
-                let current_value = get_address(&mut memory, &data_pointer);
-                set_address(&mut memory, &data_pointer, &mut(current_value + 1));
+                memory.increment_current_address(&1);
             }
             '-' => {
                 // Decrement the currently pointed byte by one
-                let current_value = get_address(&mut memory, &data_pointer);
-                set_address(&mut memory, &data_pointer, &mut(current_value - 1));
+                memory.decrement_current_address(&1);
             }
             '.' => {
                 // Convert the pointed byte to ASCII and print it
-                print!("{}", char::from(get_address(&mut memory, &data_pointer)));
+                print!("{}", char::from(memory.read_current_address()));
             }
             ',' => {
                 //TODO: implement this
             }
             '[' => {
                 // Jump foward to the next ']' if the current byte is non-zero
-                if get_address(&mut memory, &data_pointer) == 0 {
+                if memory.read_current_address() == 0 {
                     for j in i..commands.len() {
                         if commands[j] == ']' {
                             i = j + 1;
@@ -70,28 +67,5 @@ fn main() {
             _ => {} // Cover all cases
         }
         i += 1;
-    }
-}
-
-fn set_address (memory: &mut Vec<u8>, pointer: &usize, new_val: &mut u8) {
-    let val = memory.get_mut(*pointer);
-    match val {
-        Some(x) => *x = *new_val,
-        None => {
-            memory.resize(pointer + 1, 0);
-            *memory.get_mut(*pointer).unwrap() = *new_val;
-        }
-    }
-}
-
-fn get_address (memory: &mut Vec<u8>, pointer: &usize) -> u8 {
-    let val = memory.get(*pointer);
-    match val {
-        Some(x) => *x,
-        None => { 
-            // If the memory is not long enough, lengthen it
-            memory.resize(pointer + 1, 0);
-            *memory.get(*pointer).unwrap()
-        }
     }
 }
